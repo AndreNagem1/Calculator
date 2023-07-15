894519b893d090103080d908666f82bb254e4a9b
Andre Nagem <andre.nagem@shopper.com.br>
2023-07-15 17:58:57 -0300

    fixed theme import

--- app/src/main/java/com/nagem/calculator/MainActivity.kt
+++ app/src/main/java/com/nagem/calculator/MainActivity.kt
@@ -3,13 +3,13 @@ package com.nagem.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.nagem.calculator.ui.theme.CalculatorTheme
+import com.nagem.calculator.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorTheme {
+            MaterialCalculatorTheme() {
 
             }
         }
